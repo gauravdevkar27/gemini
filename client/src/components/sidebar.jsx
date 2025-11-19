d5d677414e8862bbda7eaa3f7627f5626dddd1c6
Gaurav Devkar <155799241+gauravdevkar27@users.noreply.github.com>
2025-11-19 20:40:53 +0530

    Update sidebar.jsx

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -26,12 +26,11 @@ const Sidebar = () => {
       {/* Search conversations */}
       <div className='flex items-center gap-2 p-3 mt-4 border border-gray-200
         dark:border-white/20 rounded-md bg-gray-100 dark:bg-gray-800'>
-        <img src={assets.search_icon} className='w-4 dark:invert' alt='Search icon' />
+        <img src={assets.search_icon} className='w-4 invert dark:invert-0' alt='Search icon' />
         <input onChange={(e) => setSearch(e.target.value)} value={search} type="text"
           placeholder='Search conversations' className='w-full text-xs text-gray-900 dark:text-white 
           placeholder:text-gray-400 outline-none bg-transparent' />
       </div>
-
       {/* Recent chats */}
       {chats.length > 0 && <p className='mt-4 text-sm'>Recent Chats</p>}
       <div className='flex-1 overflow-y-scroll mt-3 text-sm space-y-3'>
@@ -53,7 +52,7 @@ const Sidebar = () => {
                 </div>
                 <img
                   src={assets.bin_icon}
-                  className='hidden group-hover:block w-4 h-4 cursor-pointer dark:invert opacity-70 hover:opacity-100 transition-opacity'
+                  className='hidden group-hover:block w-4 h-4 cursor-pointer invert dark:invert-0 opacity-70 hover:opacity-100 transition-opacity'
                   alt='Delete chat'
                 />
               </div>
