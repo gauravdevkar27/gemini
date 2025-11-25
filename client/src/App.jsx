e3b31ed0d654b39a0a142812da8d4cd086f13272
gauravdevkar27 <155799241+gauravdevkar27@users.noreply.github.com>
2025-11-26 00:58:46 +0530

    added login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,10 @@ import Login from './pages/login'
 import { assets } from './assets/assets'
 import Loading from './pages/loading'
 import './assets/prism.css'
+import { useAppContext } from './context/appContext'
 const App = () => {
 
+  const {user} = useAppContext()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const {pathname} = useLocation()
 
@@ -18,7 +20,9 @@ const App = () => {
     <>
     {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3
     w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={() => setIsMenuOpen(true)}/>}
-    <div className='h-screen w-screen bg-white text-gray-900 dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+
+      {user ? (
+         <div className='h-screen w-screen bg-white text-gray-900 dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
       <div className='flex h-full w-full'>
         <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
         <Routes>
@@ -29,6 +33,14 @@ const App = () => {
         </Routes>
       </div>
     </div>
+      ) : (
+       <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex
+       items-center justify-center h-screen w-screen'>
+        <Login/>
+       </div>
+      )}
+
+   
     </>
   )
 }
