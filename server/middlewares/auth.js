7e83402e9244fcfad5d338aa64cb18cc1f90cca3
gauravdevkar27 <155799241+gauravdevkar27@users.noreply.github.com>
2025-11-30 00:12:09 +0530

    add a middleware for authetication

---
+++ server/middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user.model'
+
+
+export const prtect = async (req, res, next) => {
+    let token = req.headers.authorization
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const userId = decoded.id;
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "Not authorized, user not found"
+            });
+
+        }
+        req.user = user;
+        next()
+    } catch (error) {
+        res.status(401).json({
+            message: "Not authorized, token failed"
+        })
+    }
+}
